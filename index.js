a4f7932bd2c109aefd5207ac0ab1c5f8bc82bb1e
shohel-ahmed <shohelahmed27.sa@gmail.com>
2025-02-11 00:18:13 +0800

    added subtraction funtion

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ function addition(x,y){
     document.write("sum : " + sum + "<br>")
 }
 
+
+function subtraction(x,y){
+
+    var sub=x-y;
+    document.write("sub : " + sub + "<br>")
+}
+
 addition(20,10);
+subtraction(20,10);
